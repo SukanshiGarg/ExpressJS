1edf803c1ce12f472d49e49b3368b42beb1d67bf
Sukanshi <sukanshigargagra123@gmail.com>
2025-07-01 22:27:19 +0530

    all node js

---
+++ lecture-44/index.js
@@ -0,0 +1,28 @@
+
+const express = require('express'); //fun
+const app = express(); //obj or instance
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8080 , ()=>{
+    console.log('mera server chal gya at 8080')
+})
